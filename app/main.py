3cde7e91bee02808d7f800d53c85295f3fa53c80
Prinom2000 <prinommojumder19@gmail.com>
2025-11-18 12:49:13 +0600

    add middle ware to avoid cors error

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints import medical_report
 
 app = FastAPI(
     title="Medical Report Generator API",
     version="1.0.0",
     description="Generates structured medical reports using OpenAI and Cloudinary data."
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(medical_report.router, prefix="/api/v1", tags=["Medical Report"])
