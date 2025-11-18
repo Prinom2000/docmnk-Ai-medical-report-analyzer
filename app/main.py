5a40a12f8644248191f55308569c2e9371422f33
Prinom2000 <prinommojumder19@gmail.com>
2025-11-18 12:57:36 +0600

    add_middleware

--- app/main.py
+++ app/main.py
@@ -11,7 +11,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://docmnk.netlify.app"],
+    allow_origins=["http://localhost:5174"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
